2fa71beb79ccb44d5b57d5c6d6c2909eada03faa
larohndale <larohndale@gmail.com>
2018-04-06 02:59:44 -0400

    First Project Commit

---
+++ ToDoApp/scripts/services/data.js
@@ -0,0 +1,23 @@
+'use strict'; 
+
+angular.module("todoListApp")
+.service('dataService', function($http) {
+  this.helloWorld = function() {
+    console.log('This is a hello console service!');
+  };
+
+  this.getTodos = function(callback) {
+    $http.get('mock/todos.json')
+    .then(callback)
+  };
+
+  this.deleteTodo = function(todo) {
+    console.log("The" + todo.name + " has been deleted!")
+    // other logic
+  };
+
+  this.saveTodos = function(todos) {
+    console.log(todos.length + " todos have been saved!");
+    // other logic...
+  };
+});
